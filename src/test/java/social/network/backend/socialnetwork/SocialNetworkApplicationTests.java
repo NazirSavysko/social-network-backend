0286085ee27350ed2bc531c29acf6b19320cedbb
Nazir Savysko <nazirsavisko@gmail.com>
2025-07-12 17:21:37 +0300

    init

---
+++ src/test/java/social/network/backend/socialnetwork/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package social.network.backend.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
