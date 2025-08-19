5bb54e7c0adf1a82d0b77a33351285f68f3ce4ca
Nazir Savysko <nazirsavisko@gmail.com>
2025-08-19 16:21:15 +0300

    Remove redundant semicolons in `JwtTokenFactory` interface

--- src/main/java/social/network/backend/socialnetwork/security/jwt/JwtTokenFactory.java
+++ src/main/java/social/network/backend/socialnetwork/security/jwt/JwtTokenFactory.java
@@ -2,8 +2,6 @@ package social.network.backend.socialnetwork.security.jwt;
 
 import org.springframework.security.core.userdetails.UserDetails;
 
-;
-
 public interface JwtTokenFactory {
 
     String createToken(UserDetails userDetails);
